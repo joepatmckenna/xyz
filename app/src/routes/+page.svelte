6042faab4985b576c3ada15d9f782c453c04bd1e
Sherwood Social <joepatmckenna@gmail.com>
2023-07-19 23:07:04 -0400

    .

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -2,13 +2,27 @@
 	export let data;
 </script>
 
-<div>
+<ul>
 	{#each data.posts as post}
-		<div>
-			{post.meta.date}
+		<li>
+			<span class="date">{post.meta.date}</span>
 			<a href={post.path}>
 				{post.meta.title}
 			</a>
-		</div>
+		</li>
 	{/each}
-</div>
+</ul>
+
+<style>
+	ul {
+		list-style-type: none;
+	}
+	li {
+		padding: 12px;
+	}
+	.date {
+		color: #888;
+		font-size: 0.75em;
+		padding: 4px;
+	}
+</style>
